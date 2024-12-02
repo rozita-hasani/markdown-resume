0bfb9a621cc502f36b43eceb6754f304c53008fb
rozita-hasani <rozita.hasani74@gmail.com>
2024-12-02 14:36:48 +0100

    feat: Create Editor component

---
+++ src/components/editor/Editor.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import {useEffect, useRef} from 'react';
+import {MDXEditor, headingsPlugin, listsPlugin, quotePlugin, thematicBreakPlugin, diffSourcePlugin, MDXEditorMethods} from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+import './editor.css';
+
+interface EditorProps {
+    markdown?: string;
+    onChangeAction: (value: string) => void;
+}
+
+export default function Editor({markdown, onChangeAction}: EditorProps) {
+    const editorRef = useRef<MDXEditorMethods | null>(null)
+
+    // Sync the markdown with the editor when it changes
+    useEffect(() => {
+        if (editorRef.current && markdown !== undefined) {
+            const currentMarkdown = editorRef.current.getMarkdown();
+            if (currentMarkdown !== markdown) {
+                editorRef.current.setMarkdown(markdown);
+            }
+        }
+    }, [markdown]);
+
+    const handleChange = (newMarkdown: string) => {
+        if (markdown !== newMarkdown) {
+            onChangeAction(newMarkdown || '');
+        }
+    };
+
+    return (
+        <div className="editor m-4 relative w-1/2 custom-scrollbar overflow-auto bg-white border border-gray-200"
+            style={{height: 'calc(100vh - 32px)'}}>
+            <MDXEditor
+                ref={editorRef}
+                markdown={markdown ?? ''}
+                onChange={handleChange}
+                plugins={[
+                    headingsPlugin(),
+                    listsPlugin(),
+                    quotePlugin(),
+                    thematicBreakPlugin(),
+                    diffSourcePlugin({viewMode: 'source'})
+                ]}
+            />
+        </div>
+    );
+};
