68cb24d854d6757239cf2c6524f114e69fae4451
rozita-hasani <rozita.hasani74@gmail.com>
2024-12-04 10:10:46 +0100

    feat: remove markdown initial value

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -11,14 +11,14 @@ interface PreviewProps {
     previewContainerRef?: RefObject<HTMLDivElement>
 }
 
-export default function Preview({content = '# Hello World', theme, font, previewContainerRef}: PreviewProps) {
+export default function Preview({content, theme, font, previewContainerRef}: PreviewProps) {
     return (
         <div
             ref={previewContainerRef}
             className={`previewContainer my-4 mr-2 w-1/2 relative overflow-auto custom-scrollbar h-full theme bg-white border border-gray-200 prose max-w-none text-[#1c2024] p-3 ${theme.toLowerCase()}`}
             style={{fontFamily: font, height: 'calc(100vh - 32px)'}}
         >
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content.replace(/\\/g, "")}</ReactMarkdown>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
         </div>
     );
 };
