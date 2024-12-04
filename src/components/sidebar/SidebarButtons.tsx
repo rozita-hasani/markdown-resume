1f563e4459710aa8596e992309ebdb6c952fe704
rozita-hasani <rozita.hasani74@gmail.com>
2024-12-04 10:11:17 +0100

    feat: change name of button from download to export

--- src/components/sidebar/SidebarButtons.tsx
+++ src/components/sidebar/SidebarButtons.tsx
@@ -10,7 +10,7 @@ export const SidebarButtons = ({handlePrint}: ButtonsProps) => {
         <div className='px-4'>
             <button onClick={handlePrint} className="flex justify-center gap-2 p-3 text-white text-base font-medium bg-[#1a73e8] hover:bg-[#075cca] rounded-lg w-full transition-all ease-in-out">
                 <Download className='w-5'/>
-                Download PDF
+                Export PDF
             </button>
             <div className="mt-3 bg-[#ffdd01] mx-auto w-full flex justify-center rounded-md">
                 <a href="https://www.buymeacoffee.com/rozitahasani" target="_blank" rel="noopener noreferrer">
