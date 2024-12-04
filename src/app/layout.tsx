973b363375e959f6650a49d9db5c7aae24f2a73d
rozita-hasani <rozita.hasani74@gmail.com>
2024-12-04 10:12:11 +0100

    feat: improve style of favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,9 @@ import '../styles/globals.css';
 import React from 'react';
 
 export const metadata = {
-    title: 'Resume Markdown - Create Your Professional Resume By Markdown',
-    description: 'Create a professional resume by writing in Markdown, using our simple editor, and choose from various templates to export your resume easily.',
+    title: 'Resume Markdown',
+    description: 'Create Your Professional Resume By Markdown',
 };
-
-const base64Logo = "data:image/svg+xml;base64," +
-    btoa(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18.74"><path d="..." /></svg>`);
-
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
@@ -16,7 +12,8 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <head>
             <title>{metadata.title}</title>
             <meta name="description" content={metadata.description} />
-            <link rel="icon" href={base64Logo} />
+            <link rel="icon" href="/logo.svg" type="image/svg+xml"/>
+            <script src="/google-analytics.js" defer></script>
         </head>
         <body className='bg-gray-50 font-sans'>
         {children}
