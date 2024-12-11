1d3f71ad93c99ace0c0993022569a41b4da274d5
rozita-hasani <rozita.hasani74@gmail.com>
2024-12-11 10:27:24 +0100

    refactor: move metadata and head tag from layout to page component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,11 @@
 import '../styles/globals.css';
 import React from 'react';
 
-export const metadata = {
-    title: 'Resume Markdown',
-    description: 'Create Your Professional Resume By Markdown',
-};
-
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <head>
-            <title>{metadata.title}</title>
-            <meta name="description" content={metadata.description} />
-            <link rel="icon" href="/logo.svg" type="image/svg+xml"/>
-            <script src="/google-analytics.js" defer></script>
+            <script async type="module" src={"/google-analytics.js"}></script>
         </head>
         <body className='bg-gray-50 font-sans'>
         {children}
